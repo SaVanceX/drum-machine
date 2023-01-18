d810b4f3afb8c3e317da93c94baf841a0f7906a0
SaVance Ford <savancef@gmail.com>
2023-01-17 21:24:24 -0600

    Create drum-machine react skeleton and set up basic layout

---
+++ src/App.js
@@ -0,0 +1,43 @@
+export default function App() {
+  return (
+    <>
+      <div className="outer-container" id="drum-machine">
+        <div className="pad-container">
+          <div className="pad">
+            <span>Q</span>
+          </div>
+          <div className="pad">
+            <span>Q</span>
+          </div>
+          <div className="pad">
+            <span>Q</span>
+          </div>
+          <div className="pad">
+            <span>Q</span>
+          </div>
+          <div className="pad">
+            <span>Q</span>
+          </div>
+          <div className="pad">
+            <span>Q</span>
+          </div>
+          <div className="pad">
+            <span>Q</span>
+          </div>
+          <div className="pad">
+            <span>Q</span>
+          </div>
+          <div className="pad">
+            <span>Q</span>
+          </div>
+        </div>
+        <div className="controls-container">
+          <div className="power">Power</div>
+          <p>Display</p>
+          <div className="volume-slider">Volume Slider</div>
+          <div className="bank">Bank</div>
+        </div>
+      </div>
+    </>
+  );
+}
